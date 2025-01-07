c2d07baca809cb15c9218e25d15d11fe587fcd4c
maxfang168 <159708634+maxfang168@users.noreply.github.com>
2025-01-07 16:26:14 -0500

    Fix memory leak comment, update training file path, and clean up unused code

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#include <iostream> //possible memory lesak with higher vocab sizes; troubleshoot
+#include <iostream> //possible memory leak with higher vocab sizes; troubleshoot
 #include <string>
 #include <fstream>
 #include <vector>
@@ -14,7 +14,7 @@ std::vector<std::string> tokens;
 std::string fileContent;
 std::string processedFileContent;
 const std::unordered_set<char> punctuation = {'.', ',', '!', '?', ';', ':', '(', ')', '-', '`', '\'', '\"', '[', ']', '{', '}', '<', '>', '/', '\\', '_', '@', '=', '+', '*', '%', '^', '~', '#', '$', '&', '\n', '\t', static_cast<char>(0x92)};
-std::string trainingFilePath = "C:\\Users\\RLS\\Documents\\GitHub\\test-cpp-nlp-ann\\simplified.txt";
+std::string trainingFilePath = "C:\\Users\\RLS\\Documents\\GitHub\\test-cpp-nlp-ann\\numbers.txt";
 std::string prompt;
 std::vector<std::string> promptTokens;
 int vocabSize = 0;
@@ -73,7 +73,6 @@ std::string mapTokentoString(int tokenIndex)
 int toLowercase(std::string &text)
 {
     std::transform(text.begin(), text.end(), text.begin(), ::tolower);
-    clearVectors();
     return 0;
 }
 bool isPunctuation(char character)
@@ -113,7 +112,6 @@ int tokenizeText()
     std::cout << "Number of segments (Duplicates included): " << numSegments << std::endl;
     // Sort and remove duplicates
     std::sort(tokens.begin(), tokens.end());
-    // Use unordered_set to remove duplicates
     std::unordered_set<std::string> uniqueTokens(tokens.begin(), tokens.end());
     tokens.assign(uniqueTokens.begin(), uniqueTokens.end());
     std::cout << "Tokenizing completed." << std::endl;
@@ -177,38 +175,34 @@ int runANN(int contextTokenIndex1, int contextTokenIndex2, int contextTokenIndex
     layer1Values.clear();
     outputValues.clear();
     inputValues.clear();
-    layer1Values.resize(vocabSize*3);
-    outputValues.resize(vocabSize*3);
-    inputValues.resize(vocabSize*3);
-int initMarker = 1; // Marker dictates starting amount in one-hot encoding.
+    layer1Values.resize(vocabSize * 3);
+    outputValues.resize(vocabSize * 3);
+    inputValues.resize(vocabSize * 3);
+
+    int initMarker = 1; // Marker dictates starting amount in one-hot encoding.
     inputValues[contextTokenIndex1] = initMarker;
-    inputValues[(contextTokenIndex2 + vocabSize)] = initMarker;             // mirror to other side of context
-    inputValues[(contextTokenIndex3 + vocabSize + vocabSize)] = initMarker; // mirror to other side of context
-    for (size_t i = 0; i < vocabSize * 3; ++i)
-    {
-        inputValues[i] = inputValues[i] * inputWeights[i] + inputBiases[i];
-    }
+    inputValues[contextTokenIndex2 + vocabSize] = initMarker;             // mirror to other side of context
+    inputValues[contextTokenIndex3 + 2 * vocabSize] = initMarker; // mirror to other side of context
+
+    std::cout << "Input values initialized" << std::endl;
+
+    // Combine input and layer1 calculations
     for (size_t i = 0; i < vocabSize * 3; ++i)
     {
         layer1Values[i] = inputValues[i] * inputWeights[i] + inputBiases[i];
     }
+
+    std::cout << "Layer 1 values calculated" << std::endl;
+
+    // Calculate output values
     for (size_t i = 0; i < vocabSize * 3; ++i)
     {
         outputValues[i] = layer1Values[i] * layer1Weights[i] + layer1Biases[i];
+        outputValues[i] += layer1Values[i] * layer1Weights[i] + layer1Biases[i];
     }
-    for (size_t j = 0; j < vocabSize * 3; ++j)
-    {
-        outputValues[j] += layer1Values[j] * layer1Weights[j] + layer1Biases[j];
-    }
-    for (size_t j = 0; j < vocabSize * 3; ++j)
-    {
-        outputValues[j] -= outputBiases[j];
-    }
-    // std::cout << "Unsorted output values:" << std::endl;
 
-    // for (size_t i = 0; i < outputValues.size(); ++i)
-    //{
-    //     std::cout << "Output Value Index: " << std::to_string(i) << " | " << /outputValues[i] << std::endl;
+    std::cout << "Output values calculated" << std::endl;
+
     // Apply softmax to outputValues
     double sumExp = 0.0;
     for (auto &val : outputValues)
@@ -221,24 +215,32 @@ int initMarker = 1; // Marker dictates starting amount in one-hot encoding.
         val /= sumExp;
     }
 
+    std::cout << "Softmax applied to output values" << std::endl;
+
     auto maxElementIter = std::max_element(outputValues.begin(), outputValues.end());
     int maxElementIndex = std::distance(outputValues.begin(), maxElementIter);
+    std::cout << "Max element index: " << maxElementIndex << std::endl;
     return maxElementIndex;
 }
+
 std::string runSentence(int firstToken, int secondToken, int thirdToken, int wordLimit)
 {
+    std::cout << "Running sentence generation" << std::endl;
     int output = runANN(firstToken, secondToken, thirdToken);
     phraseTokens.push_back(output);
+    std::cout << "Initial tokens: " << firstToken << ", " << secondToken << ", " << thirdToken << std::endl;
     for (int i = 2; i < wordLimit; i++)
     {
         output = runANN(phraseTokens[i - 2], phraseTokens[i - 1], phraseTokens[i]);
         phraseTokens.push_back(output);
+        std::cout << "Generated token " << i << ": " << output << std::endl;
     }
     std::string outputString;
     for (size_t i = 0; i < wordLimit; ++i)
     {
         outputString += (" " + mapTokentoString(phraseTokens[i]));
     }
+    std::cout << "Generated sentence: " << outputString << std::endl;
     return outputString;
 }
 int runPrompt()
@@ -253,7 +255,7 @@ int runPrompt()
     printText(prompt);
     std::string newPrompt = prompt;
     std::transform(newPrompt.begin(), newPrompt.end(), newPrompt.begin(), [](char c) {
-        return isPunctuation(c) ? ' ' : c;
+        return isspace(c) ? ' ' : c;
     });
 
     prompt = newPrompt;
@@ -277,10 +279,6 @@ int runPrompt()
         std::remove_if(promptTokens.begin(), promptTokens.end(), [](const std::string &token)
                        { return token.empty(); }),
         promptTokens.end());
-    // for (size_t i = 0; i < promptTokens.size(); ++i)
-    //{
-    //     std::cout << "Prompt token Index: " << std::to_string(i) << " | " << promptTokens[i] << std::endl;
-    // }
     segmentPrompt(prompt);
     std::cout << "Prompt to tokens completed." << std::endl;
     for (size_t j = 0; j < tokenizedText.size(); ++j)
@@ -325,16 +323,6 @@ int main()
     size_t numSegments = tokens.size();
     std::cout << "Number of segments (No duplicates): " << numSegments << std::endl;
     vocabSize = numSegments; // Set the vocabulary size
-    for (size_t i = 0; i < vocabSize; ++i)
-    {
-        inputWeights.push_back(randomNumber(-1, 1));
-        inputBiases.push_back(randomNumber(-1, 1));
-        layer1Weights.push_back(randomNumber(-1, 1));
-        layer1Biases.push_back(randomNumber(-1, 1));
-        outputBiases.push_back(randomNumber(-1, 1));
-    }
-    std::cout << "Tokenizing completed." << std::endl;
-
     inputWeights.resize(vocabSize);
     inputBiases.resize(vocabSize);
     inputValues.resize(vocabSize);
@@ -343,118 +331,29 @@ int main()
     layer1Values.resize(vocabSize);
     outputBiases.resize(vocabSize);
     outputValues.resize(vocabSize);
-    bool continueRunning = true;
-    while (continueRunning)
-    {
-        {
-            /*
-                for (size_t i = 0; i < vocabSize; ++i)
-                {
-                    inputValues[i] = randomNumber(-1, 1);
-                    std::cout << "Input Value: " << std::to_string(i) << " | " << std::to_string(inputValues[i]) << std::endl;
-                    layer1Values[i] = randomNumber(-1, 1);
-                    std::cout << "Layer 1 Value: " << std::to_string(i) << " | " << std::to_string(layer1Values[i]) << std::endl;
-                    outputValues[i] = randomNumber(-1, 1);
-                    std::cout << "Output Value: " << std::to_string(i) << " | " << std::to_string(outputValues[i]) << std::endl;
-                }
-                */
-            std::cout << runSentence(0, 1, 2, 400);
-            break; // implement better solution later
-        }
-    }
-    return 0;
-}
-/* DEPRECATED
-int mainWithPrompt()
-{
-    
-    Optional boot function. It runs the prompt (single context). Note that this function may not work with more recent updates.
-    
-    std::cout << "Copyright maxfang168, 2024-2025. Prerelease version.";
 
-    std::cout << "Main funct init now." << std::endl;
-    std::ifstream file(trainingFilePath); // Open the file
-    // initializeWeights();
-    if (file.is_open())
-    { // Check if the file is open
-        std::string line;
-        while (std::getline(file, line))
-        {                        // Read each line of the file
-            fileContent += line; // Add the line to the content string
-            fileContent += "\n"; // Add a newline character after each line
-        }
-        file.close(); // Close the file
-    }
-    else
-    {
-        std::cout << "Unable to open file" << std::endl;
-        return 1;
-    }
-    preprocessText();
-
-    tokenizeText();
-    trimTokens();
-    
-    for (size_t i = 0; i < tokens.size(); ++i)
-    {
-        std::cout << "Token Index: " << std::to_string(i) << " | " << tokens[i] << std::endl;
-    }
-    
-    size_t numSegments = tokens.size();
-    std::cout << "Number of segments (No duplicates): " << numSegments << std::endl;
-    vocabSize = numSegments; // Set the vocabulary size
     for (size_t i = 0; i < vocabSize; ++i)
     {
-        inputWeights.push_back(randomNumber(-1, 1));
-        inputBiases.push_back(randomNumber(-1, 1));
-        layer1Weights.push_back(randomNumber(-1, 1));
-        layer1Biases.push_back(randomNumber(-1, 1));
-        outputBiases.push_back(randomNumber(-1, 1));
+        inputWeights[i] = randomNumber(-1, 1);
+        inputBiases[i] = randomNumber(-1, 1);
+        layer1Weights[i] = randomNumber(-1, 1);
+        layer1Biases[i] = randomNumber(-1, 1);
+        outputBiases[i] = randomNumber(-1, 1);
     }
     std::cout << "Tokenizing completed." << std::endl;
-
-    inputWeights.resize(vocabSize);
-    inputBiases.resize(vocabSize);
-    inputValues.resize(vocabSize);
-    layer1Weights.resize(vocabSize);
-    layer1Biases.resize(vocabSize);
-    layer1Values.resize(vocabSize);
-    outputBiases.resize(vocabSize);
-    outputValues.resize(vocabSize);
     bool continueRunning = true;
     while (continueRunning)
     {
-        {
-            
-                for (size_t i = 0; i < vocabSize; ++i)
-                {
-                    inputValues[i] = randomNumber(-1, 1);
-                    std::cout << "Input Value: " << std::to_string(i) << " | " << std::to_string(inputValues[i]) << std::endl;
-                    layer1Values[i] = randomNumber(-1, 1);
-                    std::cout << "Layer 1 Value: " << std::to_string(i) << " | " << std::to_string(layer1Values[i]) << std::endl;
-                    outputValues[i] = randomNumber(-1, 1);
-                    std::cout << "Output Value: " << std::to_string(i) << " | " << std::to_string(outputValues[i]) << std::endl;
-                }
-                
-            tokenizedText.clear();
-            runPrompt();
-            std::cout << "Done running runPrompt()" << std::endl;
-            int result = runANN(tokenizedText[tokenizedText.size() - 1]);
-            if (!tokenizedText.empty())
-            {
+        std::cout << runSentence(0, 1, 2, 100);
+        std::cout << "stop";
 
-                std::cout << result << std::endl;
-                std::cout << "Result: " << mapTokentoString(tokenizedText[tokenizedText.size() - 1]) << " " << mapTokentoString(result) << std::endl;
-                std::cout << std::endl
-                          << "Done running ANN." << std::endl;
-            }
-            else
-            {
-                std::cout << "Tokenized text is empty. Skipping ANN run." << std::endl;
-            }
-            tokenizedText.shrink_to_fit();
+        std::string userInput;
+        std::cout << "Do you want to continue? (yes/no): ";
+        std::getline(std::cin, userInput);
+        if (userInput != "yes")
+        {
+            continueRunning = false;
         }
     }
     return 0;
 }
-*/
